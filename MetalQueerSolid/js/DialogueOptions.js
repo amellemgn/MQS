2ad89dd95d49497938ca86ac24df0ae07935e145
Amelle <amelle.margaron@gmail.com>
2019-11-28 03:37:51 -0500

    MQS: changing the dialogue part of script

--- MetalQueerSolid/js/DialogueOptions.js
+++ MetalQueerSolid/js/DialogueOptions.js
@@ -1,10 +1,10 @@
 class DialogueOptions {
-  constructor(firstLinkOption, secondLinkOption, textBoxText, secondTextBoxText, username1, username2) {
+  constructor(firstLinkOption, secondLinkOption, textBoxText, username1, username2) {
 
     this.firstLinkOption = firstLinkOption;
     this.secondLinkOption = secondLinkOption;
     this.textBoxText = textBoxText;
-    this.secondTextBoxText = secondTextBoxText;
+    // this.textBoxText2=textBoxText2;
 
     this.username1 = username1;
     this.username2 = username2;
--- MetalQueerSolid/js/script.js
+++ MetalQueerSolid/js/script.js
@@ -2,21 +2,28 @@
 window.onload = function() {
   var blueImage = document.getElementById("blueimage");
   var c;
-  var firstLinkOption = "Pending connection...Pending connection...Connection established.";
-  var firstLinkOptionTwo = "Hey, Naked Bear. ";
-  var firstLinkArray = [firstLinkOption, firstLinkOptionTwo];
-  var secondLinkOption = "Come on, who do you think could get this frequency? I know all about you.";
-  var secondLinkOptionTwo = "> e#$Analyzing sender data@#E… FQN scan 10% complete.$%#$# > r##Analyzing sender datar3#@…. FQN scan 18% complete.o§";
-  var secondLinkOptionThree = "Reveal yourself! > Analyzing sender data….FQN scan 32% complete.";
-  var secondLinkOptionFour = "Ah, you know. That’s no fun. The thing is, I know all about you Naked Bear. You’ve got quite the reputation. Where to start….Naked Bear, killing machine extraordinaire. Million dollar merc. Give him a week and he'll set the whole country on fire. SHARKTOOTH defect. Not a Russian, not an American. Works for no man but himself. Heh. You're a wanted man, Naked Bear.> Analyzing sender data… FQN scan 68% complete….";
-  var thirdLinkOption = "this is thirdLink this is thirdLink";
-  var thirdLinkOption2 = "this is thirdLink this is thirdLinkferreofnreeorgnel";
-  var fourthLinkOption = "this is fourthLink this is fourthLink";
-  var fifthLinkOption = "this is fifthLink this is fifthLink";
-  var sixthLinkOption = "this is sixthLink this is sixthLink";
+  var option1 = "Frequency: 132.4 \n Unknown sender.\n Accept message?";
+  var option2 = "Pending connection... \nPending connection...\n Connection established. \n \n \n ??????? \n  Naked Bear... Hey.";
+  var option3 = "Really? Come on, who do you think could get this frequency? \n \n> Analyzing sender data... scan 10% complete. \n > Analyzing sender data... scan 18% complete.";
+  var option4 = "You’re no fun. \n \n > Analyzing sender data… scan 32% complete. \n \n The thing is, Naked Bear, you’ve got quite the reputation. Million dollar merc with the billion dollar bounty. UNIT_0 defect. Stabbed ‘em all in the back, the Russians, the Americans... too many times not to be working on his own… What can I say Naked Bear… you’re a wanted man.\n \n > Analyzing sender data…scan 68% complete….";
+  var option5 = "The question you should be asking instead is….. what can I do for you? \n ;) \n\n > Analyzing sender data... scan 81% complete…. \n\n  NAKED BEAR: \n Why are you contacting me?\n\n > Analyzing sender data...scan 91% complete….\n > Potential match found. Running secondary scans. \n\n ???????: \n  I’ve heard so much about you, Naked Bear. Guess the curiosity got me too hot under the collar. I just had to talk to the man in question.";
+  var option6 = "Heh.\n \n > Frequency scan complete. \n\n > Match Found: BIG DADDY";
+  var option7 = "That’s right. Come to daddy. See Bear, you’ve got something I want, and I’ve got something you need. \n\n NAKED BEAR: \n Rumours have been swirling about you too, you know. How you also just recently betrayed UNIT_0 and the US armed forces… ";
+  // var option8 = "C’mon, ask me nicely. “Please, Big Daddy?";
+  // var option9 = "That’s what I thought. Remember the last time we met? \n\n NAKED BEAR \nHow could I not. \n\n BIG DADDY \n\n ";
+  // var option10 = "sure know how to...";
+  // var option11 = "makes us men....";
+  // var option12 = "make me alive...";
+  var option13 = "I used to think that I was doing to protect those who couldn’t defend themselves. The weak, the helpless…. Big fucking man pumped full of nanotech… I’d wake up with a week of missing memories and a blood of a thousand villagers on my hands. \n\nNAKED BEAR\nThe first couple of years in UNIT_0, they congratulate you. They promote you, put you on a pedestal. Clap you on the back, the only winners in war are the people. \n\nBIG DADDY \nAnd are they? How many people you killed in this war Bear? How many civilians? Just wanted to help mankind. Turns out the ones getting used was us, hm?testtesttesttest";
+  // var option14 = "communism";
+  // var option15 = "how can i trust";
+  var option16 = "I told you, we're not so different you and I. I need you to help me stop Maximum Padernus. For Maximum Padernus and UNIT_0, N0_FUTURE is the advent of warfare. An ultimate, never-ending civilian war. Here, I'll send you the documents. If NO_FUTURE were to be implanted...";
+  var option17 = "Maximum Padernus would have access to the brains of half the global population. Over four billion people turned into mindless drones. It won't be easy to bring him down, but the alternative is unacceptable.";
+  var option18 = "";
+
   let clicks = 0;
   var dialogOptions = [];
-  var speed = 40;
+  var speed = 130;
 
   var alert = new Audio("assets/sounds/alert.mp3");
 
@@ -24,32 +31,29 @@ window.onload = function() {
     alert.play();
   }
 
-  dialogOptions.push(new DialogueOptions("Who is this?", "How Did u get this frequency?", firstLinkOption, firstLinkOptionTwo, "BIG DADDY", "???????"));
-  dialogOptions.push(new DialogueOptions("......", "Who are you?", secondLinkOption, secondLinkOptionTwo, "?????????", "NAKED BEAR"));
-  dialogOptions.push(new DialogueOptions("YEs", "No ", thirdLinkOption, thirdLinkOption2, "??????", "NAKED BEAR"));
-  dialogOptions.push(new DialogueOptions("Second4", "Third4", fourthLinkOption, "NAKED BEAR", "BIG DADDY"));
-  dialogOptions.push(new DialogueOptions("Second5", "Third5", fifthLinkOption, "BIG DADDY", "NAKED BEAR"));
-  dialogOptions.push(new DialogueOptions("Second6", "Third6", sixthLinkOption, "BIG DADDY", "NAKED BEAR"));
+  dialogOptions.push(new DialogueOptions("Yes", "", option1, "", ""));
+  dialogOptions.push(new DialogueOptions("Yes", "", option2, "", ""));
+  dialogOptions.push(new DialogueOptions("Who is this? ", "How did you get this frequency?", option3, "???????:", ""));
+  dialogOptions.push(new DialogueOptions("Reveal yourself, fool.", "Why are you contacting me?", option4, "???????", ""));
+  dialogOptions.push(new DialogueOptions("......", "Who are you? ", option5, "??????:", ""));
+  dialogOptions.push(new DialogueOptions("......", "Who are you?", option6, "??????:", ""));
+  dialogOptions.push(new DialogueOptions("!!!", "Hrngh… Big Daddy!", option7, "BIG DADDY:", ""));
+  // dialogOptions.push(new DialogueOptions("Hard to trust a guy like that.", "So what do you want from me?", option8, "BIG DADDY:", ""));
+  dialogOptions.push(new DialogueOptions("Hard to trust a guy like that.", "So what do you want from me?", option13, "BIG DADDY:", ""));
+  dialogOptions.push(new DialogueOptions("Worst thing the Nano Therapy Project did to me was remove my capacity to get drunk.", "Only time I feel alive is when I cheat death on the battlefield. [skip]", option16, "BIG DADDY:", ""));
+  dialogOptions.push(new DialogueOptions("This would be the end of civilization as we know it.", "This would mean the end, for soldiers like us.", option17, "BIG DADDY:", ""));
+
 
 
   function choseLink() {
     removeObjects();
     addFirstUserObjects();
     addTypedObjects();
-    addSecondUserObjects();
-
+    // addTyped2Objects();
 
     typeWriter(dialogOptions[clicks]);
-    //let typed = new Typed('#typed', dialogOptions[clicks].options);
-
-    /*document.getElementById("userPara1").textContent = dialogOptions[clicks].username1;
-    document.getElementById("userPara2").textContent = dialogOptions[clicks].username2;
-    document.getElementById("changeText").textContent = dialogOptions[clicks].textBoxText;
-    document.getElementById("updateFirstLink").textContent = dialogOptions[clicks].firstLinkOption;
-    document.getElementById("updateSecondLink").textContent = dialogOptions[clicks].secondLinkOption;*/
     clicks++;
 
-    //console.log("clicks");
   }
 
   function typeWriter(e) {
@@ -62,19 +66,24 @@ window.onload = function() {
           // && e.textBoxText=== dialogOptions[clicks].thirdLinkOption
           //  console.log(e.textBoxText);
           //console.log(e);
-          if (e.i == e.textBoxText.length && e.textBoxText === thirdLinkOption) {
+          if (e.i == e.textBoxText.length && e.textBoxText === option7) {
             console.log("should display blue image");
             document.getElementById("blueimage").style.display = "inline";
           }
-          if (e.i == e.textBoxText.length && e.textBoxText === secondLinkOption) {
+          if (e.i == e.textBoxText.length && e.textBoxText === option16) {
+            console.log("should display red image");
+            document.getElementById("redimage").style.display = "inline";
+          }
+          if (e.i == e.textBoxText.length && e.textBoxText === option2 || e.textBoxText === option6 ) {
             console.log("should play sound");
             playAudio();
           }
-          if (e.i == e.textBoxText.length && e.textBoxText === secondLinkOption) {
+          if (e.i == e.textBoxText.length && e.textBoxText === option6) {
             console.log("change profile pic");
             document.getElementById("profilePicture").src = "assets/images/BigDaddyGif.gif";
+            changeUsername();
           }
-          if (e.i == e.textBoxText.length && e.textBoxText === sixthLinkOption) {
+          if (e.i == e.textBoxText.length && e.textBoxText === option17) {
             console.log("end game");
             window.location.assign("endscreen.html");
           }
@@ -84,10 +93,41 @@ window.onload = function() {
 
       if (e.i == e.textBoxText.length) {
         console.log("test");
+        addSecondUserObjects();
         addFirstLinkObjects();
         addSecondLinkObjects();
+        // typeWriter2(dialogOptions[clicks]);
+        // console.log("called typeWriter2");
       }
     }
+  }
+
+  // function typeWriter2(a) {
+  //   //  console.log(e.i);
+  //   if (a.i < a.textBoxText2.length) {
+  //     document.getElementById("typed2").innerHTML += a.textBoxText2.charAt(a.i);
+  //     //console.log(e.textBoxText.charAt(e.i));
+  //     a.i++;
+  //     setTimeout(function() {
+  //         typeWriter(a);
+  //       },
+  //       a.speed);
+  //     if (a.i == a.textBoxText2.length) {
+  //       console.log("time to add link objects");
+  //       addFirstLinkObjects();
+  //       addSecondLinkObjects();
+  //     }
+  //   }
+  // }
+
+  function changeUsername(){
+  var myobj6 = document.getElementById("pictureuser1");
+  myobj6.remove();
+
+  var para6 = document.createElement("div");
+  para6.textContent = dialogOptions[clicks].username1;
+  para6.setAttribute("id", "pictureuser1");
+  document.getElementById("profilePicture1").appendChild(para6);
   }
 
   function removeObjects() {
@@ -97,11 +137,15 @@ window.onload = function() {
     myobj1.remove();
     var myobj2 = document.getElementById("typed");
     myobj2.remove();
+
+    // var myobj5 = document.getElementById("typed2");
+    // myobj5.remove();
     var myobj3 = document.getElementById("linkOne");
     myobj3.remove();
     var myobj4 = document.getElementById("linkTwo");
     myobj4.remove();
   }
+
 
 
   function addFirstUserObjects() {
@@ -126,6 +170,14 @@ window.onload = function() {
     para2.setAttribute("id", "typed");
     document.getElementById("textingBox").appendChild(para2);
   }
+
+  // function addTyped2Objects() {
+  //   console.log("called addyped 2objects");
+  //   var para5 = document.createElement("div");
+  //   //  para2.textContent = dialogOptions[clicks].textBoxText;;
+  //   para5.setAttribute("id", "typed2");
+  //   document.getElementById("textingBox").appendChild(para5);
+  // }
 
   function addFirstLinkObjects() {
     var para3 = document.createElement("div");
